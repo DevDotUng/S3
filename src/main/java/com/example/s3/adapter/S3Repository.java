ac2bc2e185f5fd45800e378aed5d5ecd1f433402
ung <wkdu4510@gmail.com>
2024-12-28 17:15:50 +0900

    코드 정리

--- src/main/java/com/example/s3/adapter/S3Repository.java
+++ src/main/java/com/example/s3/adapter/S3Repository.java
@@ -3,6 +3,4 @@ package com.example.s3.adapter;
 import com.example.s3.domain.S3Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface S3Repository extends JpaRepository<S3Image, Long> {
-
-}
+public interface S3Repository extends JpaRepository<S3Image, Long> {}
