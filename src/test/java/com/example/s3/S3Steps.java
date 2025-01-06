6f584551a2e097a9e5f34e37f91339199e560888
ung <wkdu4510@gmail.com>
2025-01-06 20:57:53 +0900

    throws문 수정

--- src/test/java/com/example/s3/S3Steps.java
+++ src/test/java/com/example/s3/S3Steps.java
@@ -3,9 +3,10 @@ package com.example.s3;
 import org.springframework.mock.web.MockMultipartFile;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 
 public class S3Steps {
-    public static MockMultipartFile 이미지_생성() throws Exception {
+    public static MockMultipartFile 이미지_생성() throws IOException {
         final String fileName = "testImage"; //파일명
         final String contentType = "png"; //파일타입
         final String filePath = "src/test/resources/testImage/"+fileName+"."+contentType; //파일경로
